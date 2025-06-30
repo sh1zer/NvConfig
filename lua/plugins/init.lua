c768b4cf3720897b34290ddf172e4988bbc15392
sh1zer <thatfellashizer@gmail.com>
2025-06-30 17:59:48 +0200

    code cleanup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -53,15 +53,7 @@ return {
       border = "single",
     }
 },
--- {
---   "ray-x/lsp_signature.nvim",
---   event = "LspAttach",
---   opts = {
---     handler_opts = {
---       border = "rounded"
---     }
---   },
--- },
+
 {
   "folke/trouble.nvim",
   opts = {
@@ -125,26 +117,6 @@ return {
     lazy = false,
     opts = {},
   },
--- {
---     "kdheepak/lazygit.nvim",
---     lazy = true,
---     cmd = {
---         "LazyGit",
---         "LazyGitConfig",
---         "LazyGitCurrentFile",
---         "LazyGitFilter",
---         "LazyGitFilterCurrentFile",
---     },
---     -- optional for floating window border decoration
---     dependencies = {
---         "nvim-lua/plenary.nvim",
---     },
---     -- setting the keybinding for LazyGit with 'keys' is recommended in
---     -- order to load the plugin when the command is run for the first time
---     keys = {
---         { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
---     }
--- },
   {
   "folke/snacks.nvim",
   priority=1000,
